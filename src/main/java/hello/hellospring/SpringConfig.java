bcfd9f3d54d4034bb7130895441efb34bf48f92c
Mintchodan <z78z@naver.com>
2023-03-27 17:06:30 +0900

    h2 DB 연결 및 jdbc 통신

--- src/main/java/hello/hellospring/SpringConfig.java
+++ src/main/java/hello/hellospring/SpringConfig.java
@@ -2,14 +2,25 @@ package hello.hellospring;
 
 // 스프링 빈 직접 등록
 
+import hello.hellospring.repository.JdbcMemberRepository;
 import hello.hellospring.repository.MemberRepository;
 import hello.hellospring.repository.MemoryMemberRepsitory;
 import hello.hellospring.service.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import javax.sql.DataSource;
+
 @Configuration
 public class SpringConfig {
+
+    private DataSource dataSource;
+
+    @Autowired
+    public SpringConfig(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
 
     @Bean
     public MemberService memberService() {
@@ -18,6 +29,10 @@ public class SpringConfig {
 
     @Bean
     public MemberRepository memberRepository() {
-        return new MemoryMemberRepsitory();
+        // return new MemoryMemberRepsitory();
+
+        // 다른 어떤 코드도 손대지 않고 MemoryMemberRepsitory -> JdbcMemberRepository로 수정함으로써 DB연결
+        // 스프링을 사용하는 이유
+        return new JdbcMemberRepository(dataSource);
     }
 }
